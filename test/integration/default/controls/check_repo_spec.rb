3269a3ae1d8307ddd4b73c8a71c78f34836dd129
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-08-14 13:04:57 +0300

    update test

--- test/integration/default/controls/check_repo_spec.rb
+++ test/integration/default/controls/check_repo_spec.rb
@@ -1,6 +1,6 @@
 control 'check_website' do
 
-  describe http('https://api.github.com/repos/VisitBG/example-kitchen') do
+  describe http('https://github.com/VisitBG/example-kitchen') do
     its('status') { should cmp 200 }
     its('body') { should match 'My awesome codebase' }
   end
