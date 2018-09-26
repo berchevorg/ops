13c6a1b50c8b3d81d03c543a19ff5a95fcd454ff
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-09-26 14:23:27 +0200

    tadaaaa

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,6 @@
 control 'check_website' do
 
-  describe http('https://api.github.com/repos/VisitBG/example-bananas') do
+  describe http('https://github.com/VisitBG/example-kitchen') do
     its('status') { should cmp 200 }
     its('body') { should match 'My awesome codebase' }
   end
